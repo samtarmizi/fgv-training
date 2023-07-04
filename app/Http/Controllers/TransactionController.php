d18f3a6b76332dc10a98fde4fce3289a829f3bca
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2023-07-04 16:52:58 +0800

    latest

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -17,4 +17,23 @@ class TransactionController extends Controller
         return view('transactions.index', compact('transactions'));
 
     }
+
+    public function create()
+    {
+        // return view resources/views/transactions/create.blade.php
+        return view('transactions.create');
+    }
+
+    public function store(Request $request)
+    {
+        //store to table transactions using Transaction model
+        $transaction = new Transaction();
+        $transaction->name = $request->name;
+        $transaction->amount = $request->amount;
+        $transaction->user_id = auth()->user()->id;
+        $transaction->save();
+
+        // return redirect to transactions index page
+        return redirect()->to('/transactions');
+    }
 }
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('User Index') }}</div>
+
+                <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                        <tr>
+                            <th scope="row">{{ $user->id }}</th>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                {{ $users->links() }}
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
