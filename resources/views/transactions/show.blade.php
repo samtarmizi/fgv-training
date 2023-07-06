e97a045ef9b947d3af69f5dfb3d6b3c4b2214328
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2023-07-06 22:01:27 +0800

    add qr code

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -17,6 +17,10 @@
                             <label>Amount</label>
                             <input type="number" name="amount" class="form-control" value="{{ $transaction->amount }}" readonly>
                         </div>
+                        <div class="form-group">
+                            <label>QR Code</label>
+                            <img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={{ route('transactions.show', $transaction) }}">
+                        </div>
                 </div>
             </div>
         </div>
